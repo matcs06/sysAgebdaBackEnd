7fcb909605c23dad8e2ce0bcdbea56b0dbc4cb4d
Mateus Costa Silva <matcs06@gmail.com>
2022-05-27 13:47:07 -0300

    adding message to test

--- src/utils/UserSecurity.ts
+++ src/utils/UserSecurity.ts
@@ -5,6 +5,6 @@ export default function verifyKey(key:string){
    //ANTONIELEM
    const createUserKey = "4c176028a04a101cba2d36ca5cbb1155"
    if(key != createUserKey){
-      throw new AppError("Invalid key to create user")
+      throw new AppError("Invalid key to create user(changelog1)")
    }
 }
