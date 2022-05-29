28044f80cdde86cf3fa5baba414b3a2610e5dace
Mateus Costa Silva <matcs06@gmail.com>
2022-05-28 22:05:27 -0300

    removing key to validate user creation

--- src/modules/users/controller/createUser/CreateUserController.ts
+++ src/modules/users/controller/createUser/CreateUserController.ts
@@ -7,13 +7,13 @@ class CreateUserController {
  
   async handle(request: Request, response: Response):Promise<Response> {
     const {
-      name, username, password, key
+      name, username, password
     } = request.body;
 
     const createUserService = container.resolve(CreateUserService)
 
     await createUserService.execute({
-      name,password, username, key
+      name,password, username
     });
 
     return response.status(201).send();
