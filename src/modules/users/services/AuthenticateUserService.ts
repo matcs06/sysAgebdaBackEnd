aabf65d8eab1e606ab424f76809856a3cdd3245f
“matcs06” <you@example.com>
2023-01-10 23:55:18 -0300

    adding more days for token to be valid

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,56 +1,56 @@
 import { inject, injectable } from "tsyringe";
 import { IUserRepository } from "../repositories/IUserRepository";
 import { sign } from "jsonwebtoken";
-import {compare} from "bcrypt"
+import { compare } from "bcrypt"
 import { AppError } from "../../../shared/errors/AppError";
 
-interface IResquest{
-   username:string;
+interface IResquest {
+   username: string;
    password: string;
 }
 
-interface IResponse{
-   user:{
+interface IResponse {
+   user: {
       name: string
       username: string;
       user_id: string;
-      business_name:string;
+      business_name: string;
    },
    token: string;
 }
 
 @injectable()
 class AuthenticateUserService {
-  
+
    constructor(
       @inject("UserRepository")
-      private UserRepository: IUserRepository){  
+      private UserRepository: IUserRepository) {
    }
-   
-   async execute({username, password}: IResquest): Promise<IResponse>{
-       
+
+   async execute({ username, password }: IResquest): Promise<IResponse> {
+
       const user = await this.UserRepository.findByName(username)
 
-      if(!user){
+      if (!user) {
          throw new AppError("Email or password incorrect")
       }
 
       const passwordMatch = await compare(password, user.password)
-      
-      if(!passwordMatch){
+
+      if (!passwordMatch) {
          throw new AppError("Email or password incorrect")
       }
 
-      const token = sign({}, "a401dd02b72c03e65c64d1ec858def08",{
+      const token = sign({}, "a401dd02b72c03e65c64d1ec858def08", {
          subject: user.id,
-         expiresIn: "1d"
+         expiresIn: "10d"
       });
 
       const tokenReturn: IResponse = {
          token,
-         user:{
+         user: {
             name: user.name,
-            username: user.username, 
+            username: user.username,
             user_id: user.id,
             business_name: user.business_name
          }
