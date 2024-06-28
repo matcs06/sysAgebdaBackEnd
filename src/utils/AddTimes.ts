0a0582549b9f7fcb702a02ecd9d9a79b36468791
“matcs06” <you@example.com>
2024-06-28 11:53:20 -0300

    adding function

--- src/utils/AddTimes.ts
+++ src/utils/AddTimes.ts
@@ -1,41 +1,41 @@
-function timeToMins(time:any) {
+function timeToMins(time: any) {
   var b = time.split(':');
-  return b[0]*60 + +b[1];
+  return b[0] * 60 + +b[1];
 }
 
 // Convert minutes to a time in format hh:mm
 // Returned value is in range 00  to 24 hrs
-function timeFromMins(mins:any) {
-  function z(n:any){return (n<10? '0':'') + n;}
-  var h = (mins/60 |0) % 24; 
+function timeFromMins(mins: any) {
+  function z(n: any) { return (n < 10 ? '0' : '') + n; }
+  var h = (mins / 60 | 0) % 24;
   var m = mins % 60;
   return z(h) + ':' + z(m);
 }
 
 // Add two times in hh:mm format
-function addTimes(t0:any, t1:any) {
-   return timeFromMins(timeToMins(t0) + timeToMins(t1));
+function addTimes(t0: any, t1: any) {
+  return timeFromMins(timeToMins(t0) + timeToMins(t1));
 }
 
 
-function uniqueArray(arr: Array<string>){
-  
-  const uniqueArray = arr.filter(function(item, pos) {
-    
-    return arr.indexOf(item) == pos ;
+function uniqueArray(arr: Array<string>) {
+
+  const uniqueArray = arr.filter(function (item, pos) {
+
+    return arr.indexOf(item) == pos;
   })
 
-  var unavailableArray:Array<string> = []
+  var unavailableArray: Array<string> = []
 
-  uniqueArray.map((value)=>{
-    if(value.split(" ").length > 1){
+  uniqueArray.map((value) => {
+    if (value.split(" ").length > 1) {
       const newValue = value.split(" ")[0]
       unavailableArray.push(newValue)
     }
   })
 
-  unavailableArray.map((value, index)=>{
-    if(uniqueArray.indexOf(value) != -1){
+  unavailableArray.map((value, index) => {
+    if (uniqueArray.indexOf(value) != -1) {
       const uniqueArrayIndex = uniqueArray.indexOf(value)
       uniqueArray.splice(uniqueArrayIndex, 1)
     }
@@ -43,7 +43,12 @@ function uniqueArray(arr: Array<string>){
 
   return uniqueArray
 
-} 
-  
+}
 
-export {addTimes, uniqueArray}
+
+function addLeadingZero(number: number) {
+  return number < 10 ? `0${number}` : `${number}`;
+}
+
+
+export { addTimes, uniqueArray, addLeadingZero }
